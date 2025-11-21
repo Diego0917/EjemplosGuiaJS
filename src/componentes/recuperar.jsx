0b1f6f0046dbe7fbd936c3df63cfebdc7aa124fd
Diego <166516606+Diego0917@users.noreply.github.com>
2025-11-20 21:10:55 -0500

    Recuperar contraseña
    Requerimiento recuperar contraseña, probando

--- src/componentes/recuperar.jsx
+++ src/componentes/recuperar.jsx
@@ -1,11 +1,17 @@
-function Contraseña() {
+import { Card, CardBody, CardTitle, CardText } from "react-bootstrap";
+function Correo() {
   return (
-    <card>
-      <cardtitle>
-        <h2>DineMeNow</h2>
-      </cardtitle>
-    </card>
+    <Card style={{ width: "18rem" }}>
+      <CardBody>
+        <CardTitle>Card Title</CardTitle>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <CardText>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </CardText>
+      </CardBody>
+    </Card>
   );
 }
 
-export default Contraseña;
+export default Correo;
