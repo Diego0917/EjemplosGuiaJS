5d6a36b8fd01f099a9c4d6d738532b05c5e039cd
Diego <166516606+Diego0917@users.noreply.github.com>
2025-11-20 20:41:14 -0500

    Creacion archivos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-import Array from "./EjemplosTaller/Array";
-import Modulo from "./EjemplosTaller/Modulo";
-import Arrowfunction from "./EjemplosTaller/Arrowfunction";
-export default function App() {
-  return (
-    <div>
-      <h1>Actividades</h1>
-      <Arrowfunction />
-      <Array />
-      <Modulo />
-    </div>
-  );
+import Contraseña from "./componentes/recuperar";
+function App() {
+  return <Contraseña />;
 }
+
+export default App;
---
+++ src/componentes/recuperar.jsx
@@ -0,0 +1,11 @@
+function Contraseña() {
+  return (
+    <card>
+      <cardtitle>
+        <h2>DineMeNow</h2>
+      </cardtitle>
+    </card>
+  );
+}
+
+export default Contraseña;
